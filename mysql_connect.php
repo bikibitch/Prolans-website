53fd570399b0abafb82ec8e2a85d0c0b56b2c90c
Анель <35295539+bikibitch@users.noreply.github.com>
2024-06-06 19:02:58 +0300

    Update and rename mysql_connect.php to php/mysql_connect.php

--- mysql_connect.php
+++
@@ -1,6 +0,0 @@
-<?php
-
-    // MySQL connection
-    $db = mysqli_connect("127.0.0.1", "t96507v3_db", "<[9}J$9H1uyXE%F", "t96507v3_db");
-    
-?>
---
+++ php/mysql_connect.php
@@ -0,0 +1,6 @@
+<?php
+
+    // MySQL connection
+    $db = mysqli_connect("127.0.0.1", "root", "1234567890", "t96507v3_db");
+    
+?>
